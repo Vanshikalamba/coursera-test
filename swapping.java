e311b9935ded27e8440caa18d2b30a9a7c9ccac9
Vanshikalamba <48330999+Vanshikalamba@users.noreply.github.com>
2020-10-01 01:29:31 +0530

    java program

---
+++ swapping.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class swapping
+{
+public static void main(String args[ ])
+{
+int a,b,c;
+Scanner sc = new Scanner(System.in);
+System.out.println("enter a and b");
+a=sc.nextInt();
+b=sc.nextInt();
+c=a;
+a=b;
+b=c;
+System.out.println(" after swapping :");
+System.out.println("a =" +a);
+System.out.println("b="+b);
+}
+}
+
